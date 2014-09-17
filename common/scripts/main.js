81180b44de67a1932564fd853eb8dbf002c0c5ef
A Pham <whatsup@andrewfam.com>
2014-09-18 08:30:53 +1000

    Add decompressed style

--- common/scripts/main.js
+++ common/scripts/main.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
 	$('.q-card').click(function(){
 		var icon = $(this);
-		console.log('q-card clicked');
+
 		if(icon.hasClass('loading')) {
 			icon.removeClass('loading');
 		} else {
 			icon.addClass('loading');
 		}
+		$('.q-card').addClass('dismissed');
+		$(this).removeClass('dismissed');
+
+
+
+
+		setTimeout(function(){
+			icon.addClass('decompressed');
+		},750, function(){
+			icon.removeClass('loading');
+		});
 	});
 });
